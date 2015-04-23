310f6eaa4a72f8bea46e34844775d6b309e98283
batakerboy <philiparon.bulleros@uap.asia>
2015-04-23 15:56:58 +0800

    Fixed merging of Biometrics with Falco

--- exceltocsv/app/models/report.rb
+++ exceltocsv/app/models/report.rb
@@ -40,27 +40,54 @@ class Report < ActiveRecord::Base
 			 		@attendance = Attendance.new
 			 		@attendance.name = @@name
 			 		newdate = date_biometrics(token[25].tr('" ', ''))
+			 		timein = token[26].tr('"', '')
+			 		if timein[0] == ' '
+			 			timein = timein[1..timein.length]
+			 		end
+			 		timeout = token[27].tr('"', '')
+			 		if timeout[0] == ' '
+			 			timeout = timeout[1..timeout.length]
+			 		end
 			 		@attendance.attendance_date = newdate
-			 		@attendance.time_in = (newdate + " " + token[26].tr('"', '')).to_datetime
-			 		newtime_out = token[27].tr('" ', '')
-			 		if newtime_out.length == 3
+			 		# @attendance.time_in = (newdate + " " + timein).to_datetime
+			 		@attendance.time_in = timein.to_time
+			 		if timeout.length == 3
 			 			@attendance.time_out = ' '
-			 		else @attendance.time_out = (newdate + " " + newtime_out).to_datetime
+			 		# else @attendance.time_out = (newdate + " " + timeout).to_datetime
+			 		else @attendance.time_out = timeout.to_time
 			 		end
 			 		@attendance.save
 				elsif check_token33 != 'nil'
 					@attendance = Attendance.new
 			 		@attendance.name = @@name
 			 		newdate = date_biometrics(token[5].tr('" ', ''))
+			 		timein = token[6].tr('" ', '')
+			 		# if timein[0] == ' '
+			 		# 	timein = timein[1..timein.length]
+			 		# end
+			 		timeout = token[7].tr('" ', '')
+			 		# if timeout[0] == ' '
+			 		# 	timeout = timeout[1..timeout.length]
+			 		# end
 			 		@attendance.attendance_date = newdate
-			 		@attendance.time_in = (newdate + " " + token[6].tr('"', '')).to_datetime
-			 		newtime_out = token[7].tr('" ', '')
-			 		if newtime_out.length == 3
+			 		# @attendance.time_in = (newdate + " " + timein).to_datetime
+			 		@attendance.time_in = timein.to_time
+			 		if timeout.nil?
 			 			@attendance.time_out = ' '
-			 		else @attendance.time_out = (newdate + " " + newtime_out).to_datetime
+			 		# else @attendance.time_out = (newdate + " " + timeout).to_datetime
+			 		else @attendance.time_out = timeout.to_time
 			 		end
 			 		@attendance.save
 				end
+				puts "========================================================="
+				puts "BIOMETRICS"
+				puts "========================================================="
+				puts "Time in: String = #{timein} \t Time = #{timein.to_time}"
+				puts "========================================================="
+				puts "Time out: String = #{timeout} \t Time = #{timeout.to_time}"
+				puts "========================================================="
+				puts "#{@attendance.name}: Time in: #{@attendance.time_in} \t Time out: #{@attendance.time_out}"
+				puts "========================================================="
 			end
 		end
 
@@ -70,33 +97,53 @@ class Report < ActiveRecord::Base
 				next if i == 0
 				next if i == 1
 				token = row.to_s.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/).flatten.compact
-				date = date_falco(token[0].tr('"[ ', ''))
 				name =  token[3].tr('"', '')[1..token[3].length-1]
+				date = date_falco(token[0].tr('"[ ', ''))
+				newtime_in = token[5].tr('" ', '')
+				newtime_out = token[6].tr('"] ', '')
+				# newdatein = (date + " " + newtime_in).to_datetime
+				# newdateout = (date + " " + newtime_out).to_datetime
 				@attendance = Attendance.find_by_sql("SELECT * FROM attendances WHERE attendance_date = '#{date}' AND name = '#{name}'")
 				if @attendance[0].nil?
 					@attendance = Attendance.new
 					@attendance.attendance_date = date
 					@attendance.name = name
-					@attendance.time_in = token[5].tr('" ', '').to_time.strftime('%H:%M:%S')
-					if token[6].squeeze(" ").strip != 'nil'
-						@attendance.time_out = token[6].tr('" ', '').to_time.strftime('%H:%M:%S')
-					else @attendance.time_out = ' '
+					# @attendance.time_in = newdatein
+					@attendance.time_in = newtime_in.to_time
+					if newtime_out.length == 3
+						@attendance.time_out = ' '
+					# else @attendance.time_out = newdateout
+					else @attendance.time_out = newtime_out.to_time
 					end
 					@attendance.save
-				else
-					time_in = token[5].tr('" ', '').to_time
-						
-					Attendance.update(@attendance[0].id, time_in: time_in) if @attendance[0].time_in > time_in
-				
-					if token[6].squeeze(" ").strip != 'nil'
-						time_out = token[6].tr('" ', '').to_time
-						if @attendance[0].time_out.nil? 
-							Attendance.update(@attendance[0].id, time_out: time_out)
-						elsif @attendance[0].time_out < time_out
-							Attendance.update(@attendance[0].id, time_out: time_out)
+
+					puts "========================================================="
+					puts "FALCO"
+					puts "========================================================="
+					puts "Time in: String = #{newtime_in} \t Time = #{newtime_in.to_time}"
+					puts "========================================================="
+					puts "Time out: String = #{newtime_out} \t Time = #{newtime_out.to_time}"
+					puts "========================================================="
+					puts "#{@attendance.name}: Time in: #{@attendance.time_in} \t Time out: #{@attendance.time_out}"
+					puts "========================================================="
+				else	
+					Attendance.update(@attendance[0].id, time_in: newtime_in.to_time) if @attendance[0].time_in.strftime('%H:%M:%S') > newtime_in.to_time.strftime('%H:%M:%S')
+					if newtime_out != 'nil'
+						if @attendance[0].time_out.nil?
+							Attendance.update(@attendance[0].id, time_out: newtime_out.to_time)
+						elsif @attendance[0].time_out.strftime('%H:%M:%S') < newtime_out.to_time.strftime('%H:%M:%S')
+							Attendance.update(@attendance[0].id, time_out: newtime_out.to_time)
 						end
-					end
-						
+					end	
+					puts "========================================================="
+					puts "FALCO"
+					puts "========================================================="
+					puts "Time in: String = #{newtime_in} \t Time = #{newtime_in.to_time}"
+					puts "========================================================="
+					puts "Time out: String = #{newtime_out} \t Time = #{newtime_out.to_time}"
+					puts "========================================================="
+					puts "#{@attendance[0].name}: Time in: #{@attendance[0].time_in.to_time} \t Time out: #{@attendance[0].time_out.to_time if !@attendance[0].time_out.nil?}"
+					puts "========================================================="
 				end
 			end
 		end
