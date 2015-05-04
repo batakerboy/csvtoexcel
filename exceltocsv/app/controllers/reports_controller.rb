6ea8ae1ce605f031728c64a9dfc7bf02a9428915
batakerboy <philiparon.bulleros@uap.asia>
2015-05-04 09:24:55 +0800

    fixed merge

--- exceltocsv/app/controllers/reports_controller.rb
+++ exceltocsv/app/controllers/reports_controller.rb
@@ -19,60 +19,66 @@ class ReportsController < ApplicationController
 	end
 
 	def download_zip
-		# File.delete(Rails.root + 'reports.zip')
+	  	File.delete(Rails.root + 'reports.zip') if File.exists?(Rails.root + 'reports.zip')
 		zip = create_zip
 	  	send_file(Rails.root.join('reports.zip'), type: 'application/zip', filename: 'reports.zip')
 	end
 
 	def create_zip
 		Zip::File.open('reports.zip', Zip::File::CREATE) { |zipfile|
-		    Employee.find_by_sql("SELECT * FROM employees ORDER BY id").each do |emp|
-		    	@requests = Request.find_by_sql("SELECT * FROM requests WHERE employee_id == '#{emp.id}'")
+		    Employee.all.each do |emp|
+		    	next if emp.falco_id.nil? && emp.biometrics_id.nil?
+		    	# @requests = Request.find_by_sql("SELECT * FROM requests WHERE employee_id == '#{emp.id}'")
 				zipfile.get_output_stream("#{emp.last_name}_#{emp.first_name}.xls") { |f| 
-					f.puts(to_csv(@requests, emp))
+					f.puts(to_csv(emp))
 				}
 			end
 		}
 	end
 
-	def to_csv(requests, emp)
+	def to_csv(emp)
 		@@hours_late = 0
 		@@times_late = 0
 		@@hours_ot = 0
 		@@times_vl = 0
 		@@times_sl = 0
+		@date_start = '2015-03-21'.to_date
+		@date_end = '2015-04-03'.to_date
+		@date = @date_start 
 		CSV.generate do |csv|
 			csv << ["iRipple, Inc."]
 			csv << ["Name: #{emp.last_name}, #{emp.first_name}"]
 			csv << ["Department: #{emp.department}"]
 			csv << ["DATE", "DAY", "TIME IN", "TIME OUT", "UT DEPARTURE", "NO OF HRS LATE", "NO OF OT HOURS", "VL", "SL", "REMARKS"]
-			requests.each do |req|
-				@attendance = Attendance.where(employee_id: emp.id, attendance_date: req.date).first
-				if !@attendance.nil? && !@attendance.time_in.nil? && (@attendance.time_in.strftime('%H:%M:%S').to_time > '08:30:00'.to_time)
-					@@hours_late = @@hours_late + ((@attendance.time_in.strftime('%H:%M:%S').to_time - '08:30:00'.to_time)/1.hour)
-					@@times_late = @@times_late + 1
+			while @date < @date_end
+				@attendance = Attendance.where(employee_id: emp.id, attendance_date: @date).first
+				@req = Request.where(employee_id: emp.id, date: @date).first
+				if !@attendance.nil? && (@attendance.time_in.strftime('%H:%M:%S').to_time > '08:30:00'.to_time)
+					@@hours_late += ((@attendance.time_in.strftime('%H:%M:%S').to_time - '08:30:00'.to_time)/1.hour)
+					@@times_late += 1
 				end
 				if !@req.nil?
-					@@hours_ot = @@hours_ot + req.ot_hours
-					@@times_vl = @@times_vl + req.vacation_leave
-					@@times_sl = @@times_sl + req.sick_leave
+					@@hours_ot += @req.ot_hours.to_d if !@req.ot_hours.nil?
+					@@times_vl += @req.vacation_leave.to_d if !@req.vacation_leave.nil?
+					@@times_sl += @req.sick_leave.to_d if !@req.sick_leave.nil?
 				end
-				    csv << [req.date.strftime('%m-%d-%Y'),
-				    	req.date.strftime('%A'), 
+				    csv << [@req.date.strftime('%m-%d-%Y'),
+				    	@req.date.strftime('%A'), 
 				    	(@attendance.time_in.to_time.strftime('%H:%M %P') if !@attendance.nil? && !@attendance.time_in.nil?), 
 				    	(@attendance.time_out.to_time.strftime('%H:%M %P') if !@attendance.nil? && !@attendance.time_out.nil?), 
-				    	req.ut_time,
+				    	@req.ut_time,
 				    	(((@attendance.time_in.strftime('%H:%M:%S').to_time - '08:30:00'.to_time)/1.hour).round(2) if !@attendance.nil? && !@attendance.time_in.nil? && @attendance.time_in.strftime('%H:%M:%S').to_time > '08:30:00'.to_time),
-				    	req.ot_hours,
-				    	req.vacation_leave,
-				    	req.sick_leave,
-				    	req.remarks]
+				    	@req.ot_hours,
+				    	@req.vacation_leave,
+				    	@req.sick_leave,
+				    	@req.remarks]
+	        	@date += 1.day
 	        end
 	        csv << ["NUMBER OF TIMES TARDY", " ", " ", " ", " ", @@times_late]
 	        csv << ["TOTAL TARDINESS", " ",  " ", " ", " ", @@hours_late.round(2)]
 	        csv << ["TOTAL OT HOURS", " ", " ", " ", " ", " ", @@hours_ot]
 	        csv << ["TOTAL LEAVES ACCUMULATED", " ", " ", " ", " ", " ", " ", @@times_vl, @@times_sl]
-		end 
+		end
 	end
 
   	def import 
