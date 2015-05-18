74dade04c326106d6a49243b49e3e483881f8bee
Fernando <josefernando.gonzales@uap.asia>
2015-05-18 16:05:21 +0800

    Removed unnecessary lines and comments. Also added a default host in setup_mail.rb

--- exceltocsv/app/models/attendance.rb
+++ exceltocsv/app/models/attendance.rb
@@ -45,14 +45,6 @@ class Attendance < ActiveRecord::Base
 			 		next if @employee.nil?
 
 			 		@attendance = Attendance.where(employee_id: @employee.id, attendance_date: date_biometrics(token[25].tr('" ', ''))).first
-			 		
-		 			puts "======================="
-					puts "BIOMETRICS"
-					# puts "Date: #{@attendance.time_out}"
-					# puts "token: #{token[27]}"
-					# puts "token.length: #{token[27].length}"
-					# puts "token: #{token[27].tr('"', '').to_time}"
-					puts "======================="
 
 			 		unless @attendance.nil?
 			 			Attendance.update(@attendance.id, time_in: token[26].tr('"', '').to_time) if @attendance.time_in.strftime('%H:%M:%S').to_time > token[26].tr('"', '').to_time
@@ -98,13 +90,6 @@ class Attendance < ActiveRecord::Base
 
 				 		@attendance.attendance_date = date_biometrics(token[5]).to_date
 				 		@attendance.time_in = token[6].tr('"', '').to_time
-
-						puts "======================="
-						puts "BIOMETRICS"
-						puts "@attendance_date: #{@attendance.attendance_date}"
-						puts "Date: #{date_biometrics(token[5])}"
-						puts "token: #{token[5]}"
-						puts "======================="
 				 		
 				 		timeout = token[7].tr('"', '')
 				 		if timeout.nil?
@@ -131,10 +116,6 @@ class Attendance < ActiveRecord::Base
 						next if @employee.nil?
 						@attendance = Attendance.where(employee_id: @employee.id, attendance_date: date).first
 						
-						puts "=========================="
-						puts "token[1]: #{token[1]}"
-						puts "=========================="
-
 						if @attendance.nil?
 							@attendance = Attendance.new
 							@attendance.employee_id = @employee.id
@@ -164,59 +145,3 @@ class Attendance < ActiveRecord::Base
 		end
 	end
 end
-
-
-
-# csvFile = CSV.open(falco.path, 'r:ISO-8859-1')
-			# csvFile.each_with_index do |row, i|
-			# 	next if i == 0
-			# 	next if i == 1
-			# 	token = row.to_s.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/).flatten.compact
-			# 	name =  token[3].tr('"', '')[1..token[3].length-1]
-			# 	last_name = name.split(", ").first
-			# 	first_name = name.split(", ").last
-			# 	date = date_falco(token[0].tr('"[ ', ''))
-			# 	newtime_in = token[5].tr('" ', '')
-			# 	newtime_out = token[6].tr('"] ', '')e
-			# 	@attendance = Attendance.find_by_sql("SELECT * FROM attendances WHERE attendance_date = '#{date}' AND last_name = '#{last_name}' AND first_name = '#{first_name}'")
-			# 	if @attendance[0].nil?
-			# 		@attendance = Attendance.new
-			# 		@attendance.attendance_date = date
-			# 		@attendance.last_name = last_name
-			# 		@attendance.first_name = first_name
-			# 		@attendance.time_in = newtime_in.to_time
-			# 		if newtime_out.length == 3
-			# 			@attendance.time_out = ' '
-			# 		else @attendance.time_out = newtime_out.to_time
-			# 		end
-			# 		@attendance.save
-
-			# 		puts "========================================================="
-			# 		puts "FALCO"
-			# 		puts "========================================================="
-			# 		puts "Time in: String = #{newtime_in} \t Time = #{newtime_in.to_time}"
-			# 		puts "========================================================="
-			# 		puts "Time out: String = #{newtime_out} \t Time = #{newtime_out.to_time}"
-			# 		puts "========================================================="
-			# 		puts "#{@attendance.last_name}, #{@attendance.first_name}: Time in: #{@attendance.time_in} \t Time out: #{@attendance.time_out}"
-			# 		puts "========================================================="
-			# 	else	
-			# 		Attendance.update(@attendance[0].id, time_in: newtime_in.to_time) if @attendance[0].time_in.strftime('%H:%M:%S') > newtime_in.to_time.strftime('%H:%M:%S')
-			# 		if newtime_out != 'nil'
-			# 			if @attendance[0].time_out.nil?
-			# 				Attendance.update(@attendance[0].id, time_out: newtime_out.to_time)
-			# 			elsif @attendance[0].time_out.strftime('%H:%M:%S') < newtime_out.to_time.strftime('%H:%M:%S')
-			# 				Attendance.update(@attendance[0].id, time_out: newtime_out.to_time)
-			# 			end
-			# 		end	
-			# 		puts "========================================================="
-			# 		puts "FALCO"
-			# 		puts "========================================================="
-			# 		puts "Time in: String = #{newtime_in} \t Time = #{newtime_in.to_time}"
-			# 		puts "========================================================="
-			# 		puts "Time out: String = #{newtime_out} \t Time = #{newtime_out.to_time}"
-			# 		puts "========================================================="
-			# 		puts "#{@attendance[0].last_name}, #{@attendance[0].first_name}: Time in: #{@attendance[0].time_in.to_time} \t Time out: #{@attendance[0].time_out.to_time if !@attendance[0].time_out.nil?}"
-			# 		puts "========================================================="
-			# 	end
-			# end
